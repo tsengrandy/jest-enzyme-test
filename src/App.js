e1519ec67cffe7b6ed5c1c9d18c514de44ebf30c
Randy Tseng <randytseng@Randys-MacBook-Air.local>
2019-07-23 11:38:26 -0700

    finished test suite

--- src/App.js
+++ src/App.js
@@ -1,13 +1,46 @@
 import React, { Component } from 'react';
+import {
+  Col,
+  Button,
+  Container,
+  Row,
+} from 'react-bootstrap'
 
 export default class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      buttonText: 'Play Game',
+      gameWon: false,
+    }
+  }
+
+  toggleResult(){
+    const newButtonText = this.state.gameWon ? "Play Game" : "Excellent!"
+    const newGameWon = this.state.gameWon ? false : true
+    this.setState({buttonText: newButtonText, gameWon: newGameWon})
+  }
 
   render () {
 
     return (
-      <div>
-          <h1>Hello</h1>
-      </div>
+      <Container>
+          <h1>Testing Example</h1>
+          <Row>
+            <Col xs={4}>
+              <Button onClick={this.toggleResult.bind(this)}>
+                {this.state.buttonText}
+              </Button>
+            </Col>
+            <Col xs={8}>
+              <span id="result-text">
+                {this.state.gameWon &&
+                <h4>Congratulations! You have won!</h4>
+              }
+              </span>
+            </Col>
+          </Row>
+      </Container>
     );
   }
 }
--- src/__tests__/App.js
+++ src/__tests__/App.js
@@ -6,7 +6,33 @@ import Adapter from 'enzyme-adapter-react-16';
 
 Enzyme.configure({ adapter: new Adapter() });
 
-it("has an h1 that says 'hello'", () => {
+test("Button starts off with label 'Play Game'", () =>{
   const app = mount(<App />)
-  expect(app.find('h1').text()).toEqual("Hello")
+  expect(app.find('button').text()).toEqual('Play Game')
+})
+test("starts out with no text in main container", ()=>{
+  const app = mount(<App />)
+  expect(app.find('#result-text').text()).toEqual('')
+})
+test("button changes to 'Excellent!' when clicked", ()=>{
+  const app = mount(<App />)
+  app.find('button').simulate('click')
+  expect(app.find('button').text()).toEqual('Excellent!')
+})
+test('displays game results when button clicked', ()=>{
+  const app = mount(<App />)
+  app.find('button').simulate('click')
+  expect(app.find('#result-text').text()).toContain('Congratulations')
+})
+test('hides game results when results visible and button clicked', ()=>{
+  const app = mount(<App />)
+  app.find('button').simulate('click')
+  app.find('button').simulate('click')
+  expect(app.find('#result-text').text()).toEqual('')
+})
+test('changes button text when button clicked a second time', ()=>{
+  const app = mount(<App />)
+  app.find('button').simulate('click')
+  app.find('button').simulate('click')
+  expect(app.find('#result-text').text()).toEqual('')
 })
